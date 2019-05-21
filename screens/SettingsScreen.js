5caed6e992851980f77511614f384824f8460a46
Petr Reshetin <preshetin@gmail.com>
2019-05-21 18:27:15 +0300

    Steal expo config screen

--- screens/SettingsScreen.js
+++ screens/SettingsScreen.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { ExpoConfigView } from '@expo/samples'
+import ConfigView from '../components/ConfigView'
 
 export default class SettingsScreen extends React.Component {
   static navigationOptions = {
@@ -7,8 +7,6 @@ export default class SettingsScreen extends React.Component {
   };
 
   render () {
-    /* Go ahead and delete ExpoConfigView and replace it with your
-     * content, we just wanted to give you a quick view of your config */
-    return <ExpoConfigView />
+    return <ConfigView />
   }
 }
