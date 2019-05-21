f69648c2216a38ebc15c8912867b726577ba25d2
Petr Reshetin <preshetin@gmail.com>
2019-05-21 15:47:00 +0300

    Rename to Links to Courses

--- navigation/MainTabNavigator.js
+++ navigation/MainTabNavigator.js
@@ -1,10 +1,9 @@
 import React from 'react'
 import { Platform } from 'react-native'
 import { createStackNavigator, createBottomTabNavigator } from 'react-navigation'
-
 import TabBarIcon from '../components/TabBarIcon'
 import HomeScreen from '../screens/HomeScreen'
-import LinksScreen from '../screens/LinksScreen'
+import CoursesScreen from '../screens/CoursesScreen'
 import VisitorScreen from '../screens/VisitorScreen'
 import SettingsScreen from '../screens/SettingsScreen'
 
@@ -26,17 +25,17 @@ HomeStack.navigationOptions = {
   )
 }
 
-const LinksStack = createStackNavigator({
-  Links: LinksScreen,
+const CoursesStack = createStackNavigator({
+  Courses: CoursesScreen,
   Visitor: VisitorScreen
 })
 
-LinksStack.navigationOptions = {
-  tabBarLabel: 'Links',
+CoursesStack.navigationOptions = {
+  tabBarLabel: 'Courses',
   tabBarIcon: ({ focused }) => (
     <TabBarIcon
       focused={focused}
-      name={Platform.OS === 'ios' ? 'ios-link' : 'md-link'}
+      name={Platform.OS === 'ios' ? 'ios-archive' : 'md-link'}
     />
   )
 }
@@ -56,7 +55,7 @@ SettingsStack.navigationOptions = {
 }
 
 export default createBottomTabNavigator({
-  LinksStack,
+  CoursesStack,
   HomeStack,
   SettingsStack
 })
--- screens/CoursesScreen.js
+++ screens/CoursesScreen.js
@@ -1,11 +1,10 @@
 import React from 'react'
 import { ScrollView, StyleSheet } from 'react-native'
-import { ExpoLinksView } from '@expo/samples'
 import VisitorsListView from '../components/VisitorsListView'
 
-export default class LinksScreen extends React.Component {
+export default class CoursesScreen extends React.Component {
   static navigationOptions = {
-    title: 'Links'
+    title: 'Courses'
   };
 
   render () {
